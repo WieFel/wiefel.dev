d44a6b5eb3403b0ef849a6d72151df13d2151a3b
Felix <felix.wielander@gmail.com>
2024-10-19 12:13:42 +0200

    Adapt project, add this person does not exist

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -5,26 +5,35 @@ export default [
     name: 'This webpage',
     description:
       'My portfolio page showing the projects I made and the work experience that I have.',
-    image: 'images/projects/wattenzaehler.jpg',
-    tags: [{ label: 'Svelte', color: 'primary' }],
+    image: 'images/sample-image.png',
+    tags: [{ label: 'Web Page', color: 'primary' }, { label: '2024', color: 'secondary' }],
     type: 'Web App',
     year: '2024',
+  },
+  {
+    name: 'This Person Does Not Exist',
+    description:
+      'Flutter app reproducing the web page thispersondoesnotexist.com, which generates not existing people using AI.',
+    image: 'images/projects/thispersondoesnotexist.jpeg',
+    tags: [{ label: 'App', color: 'primary' }, { label: '2024', color: 'secondary' }],
+    type: 'Flutter package',
+    year: '2023',
   },
   {
     name: 'Gamper-Schwienbacher',
     description:
       'Web page of the engineering office Gamper-Schwienbacher.',
     image: 'images/projects/gamper-schwienbacher.jpeg',
-    tags: [{ label: 'Svelte', color: 'primary' }],
+    tags: [{ label: 'Web Page', color: 'primary' }, { label: '2023', color: 'secondary' }],
     type: 'Flutter package',
-    year: '2021',
+    year: '2023',
   },
   {
     name: 'TinyColor 2',
     description:
       'Co-Maintainer of the community fork tinycolor2, forked from the original tinycolor Flutter package.',
     image: 'images/projects/flutter.jpeg',
-    tags: [{ label: 'Svelte', color: 'primary' }],
+    tags: [{ label: 'Flutter Package', color: 'primary' }, { label: '2021', color: 'secondary' }],
     type: 'Flutter package',
     year: '2021',
   },
@@ -33,7 +42,7 @@ export default [
     description:
       'Maintainer of number_paginator, a Flutter package for implementing pagination using page numbers.',
     image: 'images/projects/flutter.jpeg',
-    tags: [{ label: 'Svelte', color: 'primary' }],
+    tags: [{ label: 'Flutter Package', color: 'primary' }, { label: '2021', color: 'secondary' }],
     type: 'Flutter package',
     year: '2021',
   },
@@ -42,7 +51,7 @@ export default [
     description:
       'Maintainer of hsv_color_pickers, a Flutter package that offers widgets for picking the different attributes of a HSV color: hue, saturation and value.',
     image: 'images/projects/flutter.jpeg',
-    tags: [{ label: 'Svelte', color: 'primary' }],
+    tags: [{ label: 'Flutter Package', color: 'primary' }, { label: '2021', color: 'secondary' }],
     type: 'Flutter package',
     year: '2021',
   },
@@ -51,7 +60,7 @@ export default [
     description:
       'Maintainer of Gallerize, a Flutter package for creating a gallery (demo) app for developed Flutter packages/widgets.',
     image: 'images/projects/flutter.jpeg',
-    tags: [{ label: 'Svelte', color: 'primary' }],
+    tags: [{ label: 'Flutter Package', color: 'primary' }, { label: '2020', color: 'secondary' }],
     type: 'Flutter package',
     year: '2020',
   },
@@ -60,16 +69,16 @@ export default [
     description:
       'The web page of the law office Gapp Wielander Laurenza.',
     image: 'images/projects/gwl.jpeg',
-    tags: [{ label: 'Svelte', color: 'primary' }],
+    tags: [{ label: 'Web Page', color: 'primary' }, { label: '2020', color: 'secondary' }],
     type: 'Webpage',
     year: '2020',
   },
   {
     name: 'Bleckl',
     description:
-      'Re-make of WattenZähler. Point counter for the South Tyrolean card games Watten & Sockn.',
+      'Flutter app. Re-make of WattenZähler. Point counter for the South Tyrolean card games Watten & Sockn.',
     image: 'images/projects/bleckl.jpeg',
-    tags: [{ label: 'Flutter', color: 'secondary' }],
+    tags: [{ label: 'App', color: 'primary' }, { label: '2020', color: 'secondary' }],
     type: 'App',
     year: '2020',
   },
@@ -78,6 +87,7 @@ export default [
     description:
       'I am co-maintainer of the Backdrop package of Flutter Community. It implements the backdrop-functionality from the Material Design specification.',
     image: 'images/projects/flutter_community.jpeg',
+    tags: [{ label: 'Flutter Package', color: 'primary' }, { label: '2019', color: 'secondary' }],
     type: 'Flutter package',
     year: '2019',
   },
@@ -86,33 +96,34 @@ export default [
     description:
       'TUM seminar project for classifying the genre of a music track only by looking at its lyrics text, using natural language processing (NLP).',
     image: 'images/projects/deep-learning.jpeg',
-    tags: [{ label: 'Powered by Image Transmutation' }],
+    tags: [{ label: 'Seminar Project', color: 'primary' }, { label: '2019', color: 'secondary' }],
     type: 'Seminar Project',
     year: '2019',
   },
   {
     name: 'VerkehrsInfo Radio 2000',
     description:
-      'Traffic announcements in South Tyrol supported by the traffic reporting centre. Powered by Radio 2000.',
+      'Native Android app for traffic announcements in South Tyrol. Supported by the traffic reporting centre. Powered by Radio 2000.',
     image: 'images/projects/verkehrsinfo.jpeg',
+    tags: [{ label: 'App', color: 'primary' }, { label: '2014', color: 'secondary' }],
     type: 'App',
     year: '2014',
   },
   {
     name: 'dieAntenne - Sportergebnisse',
     description:
-      'South Tyrolean live football scores. Powered by Radio Die Antenne. (not officially in use anymore)',
+      'Native Android app for South Tyrolean live football scores. Powered by Radio Die Antenne.',
     image: 'images/projects/dieantenne.jpeg',
-    tags: [{ label: 'Primary Color' }, { label: 'Secondary Color', color: 'secondary' }],
+    tags: [{ label: 'App', color: 'primary' }, { label: '2013', color: 'secondary' }],
     type: 'App',
     year: '2013',
   },
   {
     name: 'WattenZähler',
     description:
-      'Point counter for the South Tyrolean card game Watten. (Now replaced by Bleckl)',
+      'Native Android point counter app for the South Tyrolean card game Watten. (Now replaced by Bleckl)',
     image: 'images/projects/wattenzaehler.jpeg',
-    tags: [{ label: 'Native Android', color: 'primary' }],
+    tags: [{ label: 'App', color: 'primary' }, { label: '2012', color: 'secondary' }],
     type: 'App',
     year: 2012,
   },
